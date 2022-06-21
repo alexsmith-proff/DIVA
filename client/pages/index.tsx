f4eaf36ea902ac8a646e72a2c8c15cd4fec7cd73
alexsmith_proff <alexsmith_proff@email.ru>
2022-06-21 16:38:54 +0300

    client: project initialization

---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import s from './index.module.scss'
+
+function Index({ tests }) {
+  return (
+    <div>
+      <MainLayout >
+      </MainLayout>
+    </div>
+  )
+}
+
+export default Index
