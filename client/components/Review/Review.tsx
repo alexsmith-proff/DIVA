7d7986f93fc398d6e6aa5bcf63b5e41b82987cc1
alexsmith_proff <alexsmith_proff@email.ru>
2022-06-27 14:24:26 +0300

    In the ReviewsSection component I made an adaptive

--- client/components/Review/Review.tsx
+++ client/components/Review/Review.tsx
@@ -1,41 +1,52 @@
-import React, from 'react'
+import React from 'react'
 import { IReview } from '../../interfaces/reviews';
 
 import s from './Review.module.scss'
 
 type ReviewProps = {
     review: IReview,
-    indexActive: number,
-    index: number
+    isDesktop: boolean, // isDesktop = true - desktop (max-width > 768px); isDesktop = false - mobile (max-width < 768px)
+    indexActive?: number,
+    index?: number
 }
 
-const Review: React.FC<ReviewProps> = ({ review, indexActive, index }) => {
+const Review: React.FC<ReviewProps> = ({ review, isDesktop, indexActive, index }) => {
     return (
-        <>
-            {
-                review.side == 'left-top' &&
-                <div className={(s.review + ' ' + s.left + ' ' + s.top) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
+        <>{
+            isDesktop ?
+                <>
+                    {
+                        review.side == 'left-top' &&
+                        <div className={(s.review + ' ' + s.left + ' ' + s.top) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
+                            {review.text}
+                        </div>
+                    }
+                    {
+                        review.side == 'right-top' &&
+                        <div className={(s.review + ' ' + s.right + ' ' + s.top) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
+                            {review.text}
+                        </div>
+                    }
+                    {
+                        review.side == 'left-bottom' &&
+                        <div className={(s.review + ' ' + s.left + ' ' + s.bottom) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
+                            {review.text}
+                        </div>
+                    }
+                    {
+                        review.side == 'right-bottom' &&
+                        <div className={(s.review + ' ' + s.right + ' ' + s.bottom) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
+                            {review.text}
+                        </div>
+                    }
+                </>
+                :
+                <div className={s.mobile}>
                     {review.text}
                 </div>
-            }
-            {
-                review.side == 'right-top' &&
-                <div className={(s.review + ' ' + s.right + ' ' + s.top) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
-                    {review.text}
-                </div>
-            }
-            {
-                review.side == 'left-bottom' &&
-                <div className={(s.review + ' ' + s.left + ' ' + s.bottom) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
-                    {review.text}
-                </div>
-            }
-            {
-                review.side == 'right-bottom' &&
-                <div className={(s.review + ' ' + s.right + ' ' + s.bottom) + (index == indexActive ? ' ' + s.active : '')} style={{ left: review.X, top: review.Y }}>
-                    {review.text}
-                </div>
-            }
+
+
+        }
         </>
 
 
--- client/components/ReviewsList/ReviewsList.tsx
+++ client/components/ReviewsList/ReviewsList.tsx
@@ -12,15 +12,25 @@ const ReviewsList: React.FC<ReviewsListProps> = ({ reviews }) => {
     const [indexActive, setIndexActive] = useState<number>(0)
     useEffect(() => {
         setInterval(() => {
-            setIndexActive(prev =>  prev < reviews.length - 1 ? prev + 1 : 0)
+            setIndexActive(prev => prev < reviews.length - 1 ? prev + 1 : 0)
         }, 10000)
     }, [])
     return (
-        <div>
-            {
-                reviews.map((item, index) => <Review review={item} indexActive={indexActive} key={item._id} index={index} />)
-            }
-        </div>
+        <>
+            <div className={s.desktop}>
+                {
+                    reviews.map((item, index) => <Review isDesktop={true} review={item} indexActive={indexActive} key={item._id} index={index} />)
+                }
+            </div>
+            <div className={s.mobile}>
+                <div className={s.wrap}>
+                    {
+                        reviews.map((item) => <Review review={item} isDesktop={false} key={item._id} />)
+                    }
+                </div>
+
+            </div>
+        </>
     );
 };
 
